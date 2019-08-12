8c229342395e86f4a3a6da5fae4458410082b6c4
jialin wu <jialin.wu.02@gmail.com>
2019-08-12 15:01:15 -0700

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+const svg = d3.select("svg");
+svg.style("background-color", "pink");
+
+const height = svg.attr("height");
+const width = svg.attr("width");
+
+// const cirlce = svg.append('circle')
+//     .attr("r", height / 2)
+//     .attr("r", 200)
+//     .attr("cx", width / 2)
+//     .attr("cy", height / 2)
+//     .attr("fill", "yellow")
+//     .attr("stroke", "black");
+
